380d8ae144bcaee59e1a030f05f36c170c915d2b
jvanspijk <jcrspijk@avans.nl>
2020-06-08 21:49:25 +0200

    velden verbergen (zodat het een voor een kan laten zien)

--- src/view/Wizard_View.js
+++ src/view/Wizard_View.js
@@ -18,16 +18,19 @@ export default class Wizard_View {
                 document.getElementById('kleding-wizard').style.display = 'inline';
                 document.getElementById('tierlantijn-wizard').style.display = 'none';
                 document.getElementById('decoratie-wizard').style.display = 'none';
+                this.#configureForm(document.getElementById('kleding-wizard'));
                 break;
             case 'Tierlantijn':
                 document.getElementById('kleding-wizard').style.display = 'none';
                 document.getElementById('tierlantijn-wizard').style.display = 'inline';
                 document.getElementById('decoratie-wizard').style.display = 'none';
+                this.#configureForm(document.getElementById('tierlantijn-wizard'));
                 break;
             case 'Decoratie':
                 document.getElementById('kleding-wizard').style.display = 'none';
                 document.getElementById('tierlantijn-wizard').style.display = 'none';
                 document.getElementById('decoratie-wizard').style.display = 'inline';
+                this.#configureForm(document.getElementById('decoratie-wizard'));
                 break;
             default:
                 document.getElementById('kleding-wizard').style.display = 'none';
@@ -35,8 +38,31 @@ export default class Wizard_View {
                 document.getElementById('decoratie-wizard').style.display = 'none';
                 break;
         }
+    }
+
+    #configureForm(form)
+    {
+
+        //object.addEventListener("input", myScript);
+        let fields = form.getElementsByTagName("UL");
+        let generalFields = document.getElementById('item-wizard').getElementsByTagName("UL");
+        for (let field of fields) {
+            field.style.display = 'none';
+        }
+        for (let field of generalFields) {
+            field.style.display = 'none';
+        }
+
+        fields[0].style.display = 'inline';
+        //TODO: laad een voor een alle input velden in een array. Voeg eventlisteners toe die style display aanzetten bij het volgende input veld.
+    }
+
+    #hideField(field)
+    {
+        console.log(field);
 
     }
+
 
     hideScreen() {
         document.getElementById('wizard').style.display = 'none';
