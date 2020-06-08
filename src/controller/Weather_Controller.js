e4311702817e62bf31fdd4e017d9fd763b2b119f
jvanspijk <jcrspijk@avans.nl>
2020-06-08 20:48:31 +0200

    nieuwe weatherbycity methode in weer controller

--- src/controller/Weather_Controller.js
+++ src/controller/Weather_Controller.js
@@ -1,8 +1,9 @@
 export default class WeatherController {
     #apiKey = '40f6ccceae954782446b3d4bb031bb70';
+    #apiUrl = 'api.openweathermap.org/data/2.5/weather?q=';
 
 
-    getWeatherByCity(city) {
+    /*getWeatherByCity(city) {
         return new Promise((resolve, reject) => {
             fetch('api.openweathermap.org/data/2.5/weather?q=' + city + ',nl&appid=' + this.#apiKey)
                 .then((res) => res.json())
@@ -17,6 +18,18 @@ export default class WeatherController {
                     reject(err);
                 });
         })
-
     }
+    */
+    getWeatherByCity(city) {
+        let url = this.#apiUrl + city + ',nl&appid=' + this.#apiKey;
+        fetch(url)
+            .then((resp) => resp.json()) // Transform the data into json
+            .then(function(data) {
+                console.log('DATA= ' + data);
+            })
+            .catch(function() {
+                console.log('error in api code url: ' + url);
+            });
+    }
+
 }
