79bcb99715d3bee83be34379f700f1b2e1c347a2
Jason van Spijk <jcrspijk+gitkraken@avans.nl>
2020-06-11 20:17:14 +0200

    Item handmatig (Wizard)

---
+++ src/model/Item.js
@@ -0,0 +1,16 @@
+export default class Item {
+    //naam, beschrijving, inkoopprijs, verkoopprijs ex btw, verkoop inc btw, minimale voorraad, huidige voorraad.
+
+    #category;
+    #name;
+    #description;
+    #buyingPrice;
+    #sellingPrice; //without taxes
+    #minimalSupply;
+    #currentSupply;
+
+    addProperty(property, value) {
+
+    }
+
+}
